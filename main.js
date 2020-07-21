4193b1f100ea077c60f2ca9b4cc0dd2cf300a88f
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-07-21 14:17:13 +0200

    get elements

---
+++ main.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("submit");
+const searchInput = document.getElementById("search");
+const randomBtn = document.getElementById("random-btn");
+const resultHeading = document.getElementById("result-heading");
+const meals = document.getElementById("meals");
+const singleMeal = document.getElementById("single-meal");
